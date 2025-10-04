d1cad75f40c09e9aae4f217bf3b2cc011a2f6f90
Kitsune7X <ryuizanagi@proton.me>
2025-10-04 19:35:46 +0900

    feat: display translation of the word when the answer is correct
    Updated app.js to show the translation of the word when the user
    answers correctly.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const answer = document.getElementById("answer");
 const result = document.getElementById("result");
 const button = document.getElementById("button");
 const money = document.getElementById("money");
+const translate = document.querySelector(".translate");
 
 // "Word"としてオブジェクトを作成 -- Create "Word" object
 // const word = {
@@ -18,9 +19,11 @@ const word = {
     honyaku: "Fox"
 }
 
+// 言葉を表示 -- Display the current word
 const text = document.querySelector(".word-display");
 text.innerHTML = `<h1>${word.kotoba}</h1>`;
 
+// 金初期化 -- Initiate Money variable
 let yen = 0;
 
 money.innerHTML = `
@@ -40,12 +43,16 @@ button.addEventListener("click", function (event) {
     checkAnswer();
 });
 
+
+
 // 金加算関数 -- Yen counting function
 function checkAnswer() {
     for (let i = 0; i < word.kotai.length; i++) {
         if (answer.value === word.kotai[i]) {
             result.innerHTML = "正解！ / Correct!";
             yen += 100;
+            // 翻訳を表示 -- Display translation of the Word
+            translate.innerHTML = `<p><em>Meanings:</em> ${word.honyaku}</p>`;
             money.innerHTML = `
             <p>金/Money:  ${yen}円/yen</p>
             `;
@@ -60,3 +67,4 @@ function checkAnswer() {
 }
 
 console.log(word.kotoba);
+console.log(word.honyaku);
